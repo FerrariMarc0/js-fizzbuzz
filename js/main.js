0b5541134a5cd16e0a46a6d11e94ee29a44ccaef
FerrariMarc0 <marco1010@live.it>
2023-02-23 16:45:28 +0100

    5

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ for(let i = 1; i <= 100; i++){
 
     if(i % 3 === 0){
         console.log('fizz');
-    } else{
+     }else if (i % 5 === 0){
+        console.log('buzz');
+     }else {
         console.log(i)
     }
-
-
 }
