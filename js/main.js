3255afc7bd4fd204e6c3d05208b20a5e40ea1333
FerrariMarc0 <marco1010@live.it>
2023-02-23 16:42:07 +0100

    3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-for(let i = 0; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
 
-    
+    if(i % 3 === 0){
+        console.log('fizz');
+    } else{
+        console.log(i)
+    }
+
+
 }
